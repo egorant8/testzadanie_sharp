4733cabc7146b1563e56f01f9b10e150a69f9539
egorant8 <51228266+egorant8@users.noreply.github.com>
2022-05-10 21:32:18 +0300

    Залив

---
+++ TestZadanie/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿
+namespace TestZadanie
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.parameter_default_value3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.parameter_default_value2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.parameter_default_value1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label1.Location = new System.Drawing.Point(13, 223);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(371, 39);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(160, 25);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(13, 39);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(351, 160);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(374, 205);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(161, 52);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Отправить";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label2.Location = new System.Drawing.Point(13, 13);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "ID терминала(ов)";
+            // 
+            // parameter_default_value3
+            // 
+            this.parameter_default_value3.Location = new System.Drawing.Point(370, 176);
+            this.parameter_default_value3.Name = "parameter_default_value3";
+            this.parameter_default_value3.Size = new System.Drawing.Size(222, 23);
+            this.parameter_default_value3.TabIndex = 5;
+            this.parameter_default_value3.Text = "parameter_default_value3";
+            this.parameter_default_value3.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label3.Location = new System.Drawing.Point(371, 156);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(174, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "parameter_default_value3";
+            this.label3.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label4.Location = new System.Drawing.Point(371, 110);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(174, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "parameter_default_value2";
+            this.label4.Visible = false;
+            // 
+            // parameter_default_value2
+            // 
+            this.parameter_default_value2.Location = new System.Drawing.Point(370, 130);
+            this.parameter_default_value2.Name = "parameter_default_value2";
+            this.parameter_default_value2.Size = new System.Drawing.Size(222, 23);
+            this.parameter_default_value2.TabIndex = 7;
+            this.parameter_default_value2.Text = "parameter_default_value2";
+            this.parameter_default_value2.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.label5.Location = new System.Drawing.Point(370, 64);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(174, 17);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "parameter_default_value1";
+            this.label5.Visible = false;
+            // 
+            // parameter_default_value1
+            // 
+            this.parameter_default_value1.Location = new System.Drawing.Point(370, 84);
+            this.parameter_default_value1.Name = "parameter_default_value1";
+            this.parameter_default_value1.Size = new System.Drawing.Size(222, 23);
+            this.parameter_default_value1.TabIndex = 9;
+            this.parameter_default_value1.Text = "parameter_default_value1";
+            this.parameter_default_value1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(608, 309);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.parameter_default_value1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.parameter_default_value2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.parameter_default_value3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox parameter_default_value3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox parameter_default_value2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox parameter_default_value1;
+    }
+}
+
---
+++ TestZadanie/Form1.cs
@@ -0,0 +1,102 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestZadanie
+{
+    public partial class Form1 : Form
+    {
+        private static string password = "part";
+        private static string login = "part";
+        private string token = "";
+        WebClient wb = new WebClient();
+
+        public Form1()
+        {
+            InitializeComponent();
+            wb.Encoding = Encoding.UTF8;
+            string resp;
+            try
+            {
+                string auth_resp = wb.DownloadString("http://178.57.218.210:198/token?login=" + login + "&password=" + password);
+                JsonDocument doc = JsonDocument.Parse(auth_resp);
+                JsonElement root = doc.RootElement;
+                token = root.GetProperty("token").GetString();
+                label1.Text = "Авторизация: успешно.";
+
+                comboBox1.Items.Clear();
+                resp = wb.DownloadString("http://178.57.218.210:198/commands/types?token=" + token);
+                JsonDocument doc1 = JsonDocument.Parse(resp);
+                JsonElement root1 = doc1.RootElement;
+                foreach (var item in root1.GetProperty("items").EnumerateArray())
+                {
+                    //Console.WriteLine(item);
+                    comboBox1.Items.Add(item.GetProperty("name").GetString());
+                }
+            }
+            catch (Exception)
+            {
+                label1.Text = "Авторизация: не прошла.";
+            }
+        }
+
+        void hidden(TextBox pr1, Label lr1, TextBox Hpr2, Label Hlr2, TextBox Hpr3, Label Hlr3)
+        {
+            pr1.Visible = false;
+            lr1.Visible = false;
+            Hpr2.Visible = false;
+            Hlr2.Visible = false;
+            Hpr3.Visible = false;
+            Hlr3.Visible = false;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (token != "")
+            {
+                hidden(parameter_default_value1, label3, parameter_default_value2, label4, parameter_default_value3, label4);
+                string resp = wb.DownloadString("http://178.57.218.210:198/commands/types?token=" + token);
+                JsonDocument doc = JsonDocument.Parse(resp);
+                JsonElement root = doc.RootElement;
+                foreach (var item in root.GetProperty("items").EnumerateArray())
+                {
+                    if (comboBox1.Text == item.GetProperty("name").GetString())
+                    {
+                        MessageBox.Show(item.GetProperty("parameter_name1").ToString());
+                        if (item.GetProperty("parameter_name1").ToString() != "")
+                        {
+                            parameter_default_value1.Text = Convert.ToString(item.GetProperty("parameter_default_value1"));
+                            label5.Text = Convert.ToString(item.GetProperty("parameter_name1"));
+                            parameter_default_value1.Visible = true;
+                            label5.Visible = true;
+                        }
+                        if (item.GetProperty("parameter_name2").ToString() != "")
+                        {
+                            parameter_default_value2.Text = Convert.ToString(item.GetProperty("parameter_default_value2"));
+                            label4.Text = Convert.ToString(item.GetProperty("parameter_name2"));
+                            parameter_default_value2.Visible = true;
+                            label4.Visible = true;
+                        }
+                        if (item.GetProperty("parameter_name3").ToString() != "")
+                        {
+                            parameter_default_value3.Text = Convert.ToString(item.GetProperty("parameter_default_value3"));
+                            label3.Text = Convert.ToString(item.GetProperty("parameter_name3"));
+                            parameter_default_value3.Visible = true;
+                            label3.Visible = true;
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
